bdc0b79445cb8966f04fa67626ed4992bf490a39
Axanimander <sanderson86@mail.csuchico.edu>
2021-06-21 22:16:26 -0700

    Begin prefix tree

--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import unittest
 from pngreader import *
+from prefixtrie import *
 class testPng(unittest.TestCase):
     def test_header(self):
         z = pngReader("RED.png")
         z.get_header_string()
         self.assertEqual(z.header_string, 'IHDR')
         z.end()
+    def test_prefix(self):
+        z = Node()
 
 
 if __name__ == '__main__':
